8b00636ea72f3055dc7ebd32fcca417ba6cce9a0
Marcus Holl <marcus.holl@sap.com>
2017-06-22 15:47:50 +0200

    Read password from stdin.

--- modules/cli/src/main/java/sap/prd/cmintegration/cli/GetChangeStatus.java
+++ modules/cli/src/main/java/sap/prd/cmintegration/cli/GetChangeStatus.java
@@ -1,5 +1,9 @@
 package sap.prd.cmintegration.cli;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
@@ -13,8 +17,7 @@ public class GetChangeStatus {
         Options options = new Options();
         options.addRequiredOption("c", "change", true, "Retrieves a change.");
         options.addRequiredOption("u", "user", true, "User.");
-        options.addRequiredOption("p", "password", true, "Password");
-        // REVISIT we should not deal with a password like in the line above. Maybe read via stdin?
+        options.addRequiredOption("p", "password", true, "Password, if '-' if provided, password is read from stdin.");
         options.addRequiredOption("h", "host", true, "Host");
 
         CommandLine commandLine = new DefaultParser().parse(options, args);
@@ -22,9 +25,14 @@ public class GetChangeStatus {
         String changeId = commandLine.getOptionValue('c');
         String user = commandLine.getOptionValue('u');
         String password = commandLine.getOptionValue('p');
+        if(password.equals("-")) password = readPassword();
         String host = commandLine.getOptionValue('h');
 
         System.out.println(new CMODataClient(host, user, password).getChange(changeId).getStatus());
     }
 
+    private static String readPassword() throws IOException {
+        return new BufferedReader(
+                new InputStreamReader(System.in, "UTF-8")).readLine();
+    }
 }
