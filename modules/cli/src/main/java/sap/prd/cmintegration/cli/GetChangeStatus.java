c4fabfd39839b582e8c6820158df6d3e6d78af01
Marcus Holl <marcus.holl@sap.com>
2017-06-22 13:45:56 +0200

    [fix] Shrink the code.

--- modules/cli/src/main/java/sap/prd/cmintegration/cli/GetChangeStatus.java
+++ modules/cli/src/main/java/sap/prd/cmintegration/cli/GetChangeStatus.java
@@ -2,7 +2,6 @@ package sap.prd.cmintegration.cli;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
 import sap.ai.st.cm.plugins.ciintegration.odataclient.CMODataClient;
@@ -11,22 +10,12 @@ public class GetChangeStatus {
 
     public final static void main(String[] args) throws Exception {
 
-        Option optChange = new Option("c", "change", true, "Retrieves a change.");
-        Option optUser = new Option("u", "user", true, "User.");
-        Option optPassword = new Option("p", "password", true, "Password");
-        // REVISIT we should not deal with a password like in the line above. Maybe read via stdin?
-        Option optHost = new Option("h", "host", true, "Host");
-
-        optChange.setRequired(true);
-        optUser.setRequired(true);
-        optPassword.setRequired(true);
-        optHost.setRequired(true);
-
         Options options = new Options();
-        options.addOption(optChange);
-        options.addOption(optUser);
-        options.addOption(optPassword);
-        options.addOption(optHost);
+        options.addRequiredOption("c", "change", true, "Retrieves a change.");
+        options.addRequiredOption("u", "user", true, "User.");
+        options.addRequiredOption("p", "password", true, "Password");
+        // REVISIT we should not deal with a password like in the line above. Maybe read via stdin?
+        options.addRequiredOption("h", "host", true, "Host");
 
         CommandLine commandLine = new DefaultParser().parse(options, args);
 
