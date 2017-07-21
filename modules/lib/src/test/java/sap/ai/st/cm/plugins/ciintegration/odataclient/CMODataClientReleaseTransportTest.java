4eef7493daea3613cd7bf0abd57320d88cc50f23
Marcus Holl <marcus.holl@sap.com>
2017-07-21 11:12:29 +0200

    Provide straight-forward test for releasing a transport.

--- modules/lib/src/test/java/sap/ai/st/cm/plugins/ciintegration/odataclient/CMODataClientReleaseTransportTest.java
+++ modules/lib/src/test/java/sap/ai/st/cm/plugins/ciintegration/odataclient/CMODataClientReleaseTransportTest.java
@@ -5,17 +5,22 @@ import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.createMockBuilder;
 import static org.easymock.EasyMock.eq;
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.EasyMock.replay;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static sap.ai.st.cm.plugins.ciintegration.odataclient.MockHelper.getConfiguration;
 
 import org.apache.olingo.client.api.ODataClient;
 import org.apache.olingo.client.api.communication.ODataClientErrorException;
 import org.apache.olingo.client.api.communication.request.invoke.InvokeRequestFactory;
 import org.apache.olingo.client.api.communication.request.invoke.ODataInvokeRequest;
+import org.apache.olingo.client.api.communication.response.ODataInvokeResponse;
 import org.apache.olingo.client.api.domain.ClientEntity;
 import org.apache.olingo.client.core.ODataClientImpl;
 import org.easymock.Capture;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -34,12 +39,33 @@ public class CMODataClientReleaseTransportTest extends CMODataClientBaseTest {
     }
 
     @Test
+    public void testReleaseTransportStraightForward() throws Exception {
+
+        /*
+         * comment line below for testing against real backend.
+         *
+         * Of course against the real back-end it works only once. A transport containing at least
+         * one item in the object list needs to be prepared in advance. Other test might be used
+         * for that.
+         *
+         * Assert for the captures below needs to be commented also in this case.
+         */
+        setMock(examinee, setupMock());
+
+        examinee.releaseDevelopmentTransport("8000042445", "L21K90002K");
+
+        Assert.assertThat(address.getValue().toASCIIString(),
+                is(equalTo("https://example.org/endpoint/releaseTransport?ChangeID='8000042445'&TransportID='L21K90002K'")));
+    }
+
+    @Test
     public void testReleaseTransportFailsDueTransportHasAlreadyBeenReleased() throws Exception {
 
         thrown.expect(ODataClientErrorException.class);
         thrown.expectMessage("400"); // TODO: 404 would be better
 
-        setMock(examinee, setupExceptionMock());
+        setMock(examinee, setupMock(new ODataClientErrorException(
+                StatusLines.BAD_REQUEST)));
         examinee.releaseDevelopmentTransport("8000038673", "L21K900026");
     }
 
@@ -49,7 +75,8 @@ public class CMODataClientReleaseTransportTest extends CMODataClientBaseTest {
         thrown.expect(ODataClientErrorException.class);
         thrown.expectMessage("400"); // TODO: 404 would be better
 
-        setMock(examinee, setupExceptionMock());
+        setMock(examinee, setupMock(new ODataClientErrorException(
+                StatusLines.BAD_REQUEST)));
         examinee.releaseDevelopmentTransport("CHANGE_ID_DOES_NOT_EXIST", "TRANSPORT_REQUEST_DOES_ALSO_NOT_EXIST");
     }
 
@@ -59,7 +86,8 @@ public class CMODataClientReleaseTransportTest extends CMODataClientBaseTest {
         thrown.expect(ODataClientErrorException.class);
         thrown.expectMessage("400"); // TODO: 404 would be better
 
-        setMock(examinee, setupExceptionMock());
+        setMock(examinee, setupMock(new ODataClientErrorException(
+                StatusLines.BAD_REQUEST)));
 
         examinee.releaseDevelopmentTransport("8000038673", "DOES_NOT_EXIST");
     }
@@ -78,14 +106,26 @@ public class CMODataClientReleaseTransportTest extends CMODataClientBaseTest {
         examinee.releaseDevelopmentTransport("xx", "");
     }
 
+    private ODataClient setupMock() {
+        return setupMock(null);
+    }
+
     @SuppressWarnings("unchecked")
-    private ODataClient setupExceptionMock() {
+    private ODataClient setupMock(Exception e) {
 
         ODataInvokeRequest<ClientEntity> functionInvokeRequest = createMock(ODataInvokeRequest.class);
         expect(functionInvokeRequest.setAccept(capture(contentType))).andReturn(functionInvokeRequest);
-        expect(functionInvokeRequest.execute())
-            .andThrow(new ODataClientErrorException(
-                StatusLines.BAD_REQUEST));
+
+        if(e != null) {
+            expect(functionInvokeRequest.execute())
+                .andThrow(e);
+        } else {
+            ODataInvokeResponse<ClientEntity> responseMock = createMock(ODataInvokeResponse.class);
+            expect(responseMock.getStatusCode()).andReturn(200);
+            responseMock.close(); expectLastCall();
+            replay(responseMock);
+            expect(functionInvokeRequest.execute()).andReturn(responseMock);
+        }
 
         InvokeRequestFactory invokeRequestFactoryMock = createMock(InvokeRequestFactory.class);
         expect(invokeRequestFactoryMock.getFunctionInvokeRequest(capture(address), eq(ClientEntity.class))).andReturn(functionInvokeRequest);
