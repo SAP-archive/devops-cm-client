1d0c1c47ac76e9ec433bef51bd5695a3d340692a
Marcus Holl <marcus.holl@sap.com>
2018-03-13 09:06:07 +0100

    Constructor and getTransport method

--- modules/libv2/src/main/java/com/sap/cmclient/http/CMODataClient.java
+++ modules/libv2/src/main/java/com/sap/cmclient/http/CMODataClient.java
@@ -4,8 +4,10 @@ import static com.google.common.primitives.Ints.asList;
 import static org.apache.http.HttpStatus.SC_NOT_FOUND;
 import static org.apache.http.HttpStatus.SC_OK;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
@@ -17,11 +19,35 @@ import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.olingo.odata2.api.edm.Edm;
+import org.apache.olingo.odata2.api.edm.EdmException;
 import org.apache.olingo.odata2.api.ep.EntityProvider;
+import org.apache.olingo.odata2.api.ep.EntityProviderException;
 import org.apache.olingo.odata2.api.ep.EntityProviderReadProperties;
 import org.apache.olingo.odata2.api.ep.entry.ODataEntry;
 
 public class CMODataClient {
+
+    private final URI endpoint;
+    private final HttpClientFactory clientFactory;
+
+    public CMODataClient(String endpoint, String user, String password) throws URISyntaxException {
+
+        this.endpoint = new URI(endpoint);
+
+        // the same instance needs to be used as long as we are in the same session. Hence multiple
+        // clients must share the same cookie store. Reason: we are logged on with the first request
+        // and get a cookie. That cookie - expressing the user has already been logged in - needs to be
+        // present in all subsequent requests.
+        CookieStore sessionCookieStore = new BasicCookieStore();
+
+        BasicCredentialsProvider basicCredentialsProvider = new BasicCredentialsProvider();
+        basicCredentialsProvider.setCredentials(
+                new AuthScope(this.endpoint.getHost(), this.endpoint.getPort()),
+                new UsernamePasswordCredentials(user, password));
+
+        this.clientFactory = new HttpClientFactory(sessionCookieStore, basicCredentialsProvider);
+    }
+
 
     // args[0] "http://wdflbmd16301.wdf.sap.corp:8000/sap/opu/odata/SAP/SCTS_CLOUD_API_ODATA_SRV/Transports(\'A5DK900014\')"
     // args[1] ODATA
@@ -35,22 +61,12 @@ public class CMODataClient {
     // -Dorg.apache.commons.logging.simplelog.log.org.apache.http.wire=DEBUG
 
     public final static void main(String[] args) throws Exception {
+        new CMODataClient(args[0], args[1], args[2]).getTransport();
+    }
 
-        URI endpoint = new URI(args[0]);
+    public void getTransport() throws IOException, EntityProviderException, EdmException {
 
-        // the same instance needs to be used as long as we are in the same session. Hence multiple
-        // clients must share the same cookie store. Reason: we are logged on with the first request
-        // and get a cookie. That cookie - expressing the user has already been logged in - needs to be
-        // present in all subsequent requests.
-        CookieStore sessionCookieStore = new BasicCookieStore();
-
-        BasicCredentialsProvider basicCredentialsProvider = new BasicCredentialsProvider();
-        basicCredentialsProvider.setCredentials(
-                new AuthScope(endpoint.getHost(), endpoint.getPort()),
-                new UsernamePasswordCredentials(args[1], args[2]));
-
-        HttpClientFactory clientFactory = new HttpClientFactory(sessionCookieStore, basicCredentialsProvider);
-
+        
         final Edm edm;
         try (CloseableHttpClient edmClient = clientFactory.createClient()) {
 
