e74fded456246a7fd64fea064065451e93ad93ee
Thorsten Duda <thorsten.duda@sap.com>
2018-03-13 14:50:56 +0100

    TransportRequestBuilder

---
+++ modules/libv2/src/main/java/com/sap/cmclient/dto/TransportRequestBuilder.java
@@ -0,0 +1,64 @@
+package com.sap.cmclient.dto;
+
+import java.net.URI;
+
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+
+public class TransportRequestBuilder
+{
+
+  private final URI endpoint;
+  private static final String SEPARATOR = "/";
+  private static final String TRANSPORTS = "Transports";
+
+  public TransportRequestBuilder(URI endpoint)
+  {
+    this.endpoint = endpoint;
+  }
+
+  public HttpGet getTransport(String id)
+  {
+    HttpGet get = new HttpGet(createTransportUri(id, null));
+    return get;
+  }
+
+  public HttpPost createTransport(Transport trans)
+  {
+    return null;
+
+  }
+
+  public HttpPut updateTransport(Transport trans)
+  {
+    return null;
+
+  }
+
+  public HttpDelete deleteTransport(String id)
+  {
+    return null;
+
+  }
+
+  private String createTransportUri(String id, String expand)
+  {
+    final StringBuilder absolutUri = new StringBuilder(endpoint.toASCIIString()).append(SEPARATOR)
+      .append(TRANSPORTS);
+    if (id != null) {
+      absolutUri.append("('").append(id).append("')");
+    }
+    if (expand != null) {
+      absolutUri.append("/?$expand=").append(expand);
+    }
+    return absolutUri.toString();
+  }
+
+  public static String getEntityKey()
+  {
+    return TRANSPORTS;
+  }
+
+}
--- modules/libv2/src/main/java/com/sap/cmclient/http/CMODataClient.java
+++ modules/libv2/src/main/java/com/sap/cmclient/http/CMODataClient.java
@@ -9,6 +9,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -26,83 +28,94 @@ import org.apache.olingo.odata2.api.ep.EntityProviderReadProperties;
 
 import com.sap.cmclient.dto.Transport;
 import com.sap.cmclient.dto.TransportMarshaller;
+import com.sap.cmclient.dto.TransportRequestBuilder;
 
-public class CMODataClient {
+public class CMODataClient
+{
 
-    private final URI endpoint;
-    private final HttpClientFactory clientFactory;
+  private final URI endpoint;
+  private final HttpClientFactory clientFactory;
 
-    public CMODataClient(String endpoint, String user, String password) throws URISyntaxException {
+  public CMODataClient(String endpoint, String user, String password) throws URISyntaxException
+  {
 
-        this.endpoint = new URI(endpoint);
+    this.endpoint = new URI(endpoint);
 
-        // the same instance needs to be used as long as we are in the same session. Hence multiple
-        // clients must share the same cookie store. Reason: we are logged on with the first request
-        // and get a cookie. That cookie - expressing the user has already been logged in - needs to be
-        // present in all subsequent requests.
-        CookieStore sessionCookieStore = new BasicCookieStore();
+    // the same instance needs to be used as long as we are in the same session.
+    // Hence multiple
+    // clients must share the same cookie store. Reason: we are logged on with the
+    // first request
+    // and get a cookie. That cookie - expressing the user has already been logged
+    // in - needs to be
+    // present in all subsequent requests.
+    CookieStore sessionCookieStore = new BasicCookieStore();
 
-        BasicCredentialsProvider basicCredentialsProvider = new BasicCredentialsProvider();
-        basicCredentialsProvider.setCredentials(
-                new AuthScope(this.endpoint.getHost(), this.endpoint.getPort()),
-                new UsernamePasswordCredentials(user, password));
+    BasicCredentialsProvider basicCredentialsProvider = new BasicCredentialsProvider();
+    basicCredentialsProvider.setCredentials(new AuthScope(this.endpoint.getHost(), this.endpoint.getPort()),
+          new UsernamePasswordCredentials(user, password));
 
-        this.clientFactory = new HttpClientFactory(sessionCookieStore, basicCredentialsProvider);
+    this.clientFactory = new HttpClientFactory(sessionCookieStore, basicCredentialsProvider);
+  }
+
+  // args[0]
+  // "http://wdflbmd16301.wdf.sap.corp:8000/sap/opu/odata/SAP/SCTS_CLOUD_API_ODATA_SRV/Transports(\'A5DK900014\')"
+  // args[1] ODATA
+  // args[2] <the password ...>
+
+  // In order to run it with wire logging use:
+  // -Dorg.slf4j.simpleLogger.defaultLogLevel=trace
+  // -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog
+  // -Dorg.apache.commons.logging.simplelog.showdatetime=true
+  // -Dorg.apache.commons.logging.simplelog.log.org.apache.http=DEBUG
+  // -Dorg.apache.commons.logging.simplelog.log.org.apache.http.wire=DEBUG
+
+  public final static void main(String[] args) throws Exception
+  {
+    Transport transport = new CMODataClient(args[0], args[1], args[2]).getTransport("A5DK900014");
+    System.out.println(transport);
+  }
+
+  public Transport getTransport(String transportId) throws IOException, EntityProviderException, EdmException
+  {
+
+    final TransportRequestBuilder builder = new TransportRequestBuilder(endpoint);
+
+    try (CloseableHttpClient client = clientFactory.createClient()) {
+      HttpUriRequest get = builder.getTransport(transportId);
+      HttpResponse response = client.execute(get);
+      Header[] contentType = response.getHeaders(HttpHeaders.CONTENT_TYPE);
+      checkStatusCode(response, SC_OK, SC_NOT_FOUND, 500); // 500 is currently returned in case the transport
+      // cannot be found.
+      if (Arrays.asList(SC_OK).contains(response.getStatusLine().getStatusCode())) {
+        return TransportMarshaller.get(EntityProvider.readEntry(contentType[0].getValue(),
+              getEntityDataModel().getDefaultEntityContainer()
+                .getEntitySet(TransportRequestBuilder.getEntityKey()),
+              response.getEntity().getContent(), EntityProviderReadProperties.init().build()));
+      }
+
+      return null;
     }
+  }
 
+  private Edm getEntityDataModel() throws IOException, EntityProviderException
+  {
 
-    // args[0] "http://wdflbmd16301.wdf.sap.corp:8000/sap/opu/odata/SAP/SCTS_CLOUD_API_ODATA_SRV/Transports(\'A5DK900014\')"
-    // args[1] ODATA
-    // args[2] <the password ...>
-    
-    // In order to run it with wire logging use:
-    // -Dorg.slf4j.simpleLogger.defaultLogLevel=trace
-    // -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog
-    // -Dorg.apache.commons.logging.simplelog.showdatetime=true
-    // -Dorg.apache.commons.logging.simplelog.log.org.apache.http=DEBUG
-    // -Dorg.apache.commons.logging.simplelog.log.org.apache.http.wire=DEBUG
+    try (CloseableHttpClient edmClient = clientFactory.createClient()) {
 
-    public final static void main(String[] args) throws Exception {
-        Transport transport = new CMODataClient(args[0], args[1], args[2]).getTransport("A5DK900014");
-        System.out.println(transport);
+      return EntityProvider.readMetadata(edmClient
+        .execute(new HttpGet(endpoint.toASCIIString() + "/" + "$metadata")).getEntity().getContent(),
+            false);
     }
+  }
 
-    public Transport getTransport(String transportId) throws IOException, EntityProviderException, EdmException {
+  private static void checkStatusCode(HttpResponse response, int... expected)
+  {
 
-        final String entityKey = "Transports";
-
-        try (CloseableHttpClient client = clientFactory.createClient()) {
-            HttpUriRequest get = new HttpGet(endpoint + "/" + entityKey + "('" + transportId + "')");
-            HttpResponse response = client.execute(get);
-            checkStatusCode(response, SC_OK, SC_NOT_FOUND, 500); // 500 is currently returned in case the transport cannot be found.
-            if(Arrays.asList(SC_OK).contains(response.getStatusLine().getStatusCode())) {
-                return TransportMarshaller.get(EntityProvider.readEntry("application/xml",
-                                        getEntityDataModel().getDefaultEntityContainer().getEntitySet(entityKey),
-                                        response.getEntity().getContent(),
-                                        EntityProviderReadProperties.init().build()));
-            }
-
-            return null;
-        }
+    if (!asList(expected).contains(response.getStatusLine().getStatusCode())) {
+      // TODO: maybe dedicated exception ...
+      throw new RuntimeException(
+            String.format("Unexpected response code %d", response.getStatusLine().getStatusCode()));
     }
-
-    private Edm getEntityDataModel() throws IOException, EntityProviderException {
-
-        try (CloseableHttpClient edmClient = clientFactory.createClient()) {
-
-            return EntityProvider.readMetadata(
-                   edmClient.execute(
-                       new HttpGet(endpoint.toASCIIString() + "/" + "$metadata")
-                   ).getEntity().getContent(), false);
-        }
-    }
-
-    private static void checkStatusCode(HttpResponse response, int...  expected) {
-
-        if( ! asList(expected).contains(response.getStatusLine().getStatusCode())) {
-            // TODO: maybe dedicated exception ...
-            throw new RuntimeException(String.format("Unexpected response code %d", response.getStatusLine().getStatusCode()));
-        }
-    }
+  }
 
 }
