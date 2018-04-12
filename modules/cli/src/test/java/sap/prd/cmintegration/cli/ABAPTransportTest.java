e67bb49d7e3b3f39a1a5bc8d1ba0cb08ebfb05db
Marcus Holl <marcus.holl@sap.com>
2018-04-12 08:51:30 +0200

    Introduce dedicate test class for generic abap transports
    endpoint, user, passwd

--- modules/cli/src/test/java/sap/prd/cmintegration/cli/ABAPTransportTest.java
+++ modules/cli/src/test/java/sap/prd/cmintegration/cli/ABAPTransportTest.java
@@ -34,67 +34,6 @@ public class ABAPTransportTest extends ABAPBackendTest {
         return factoryMock;
     }
 
-    @Test
-    public void testServerCoordinatesStraightForward() throws Exception {
-
-        Map<String, Object> m = Maps.newHashMap();
-        m.put("Id", "999");
-        setMock(setupGetTransportMock(new Transport(m)));
-
-        Commands.main(new String[]
-                {       "-e", "http://example.org:8000/endpoint",
-                        "-u", "me",
-                        "-p", "openSesame",
-                        "-t", "ABAP",
-                        "get-transport-owner",
-                        "-tID", "999"});
-
-        assertThat(host.getValue(), is(equalTo("http://example.org:8000/endpoint")));
-        assertThat(user.getValue(), is(equalTo("me")));
-        assertThat(password.getValue(), is(equalTo("openSesame")));
-    }
-
-    @Test
-    public void testUserNotProvided() throws Exception {
-
-        thrown.expect(MissingOptionException.class);
-        thrown.expectMessage("Missing required option: u");
-
-        Commands.main(new String[]
-                {       "-e", "http://example.org:8000/endpoint",
-                        "-p", "openSesame",
-                        "-t", "ABAP",
-                        "get-transport-owner",
-                        "-tID", "999"});
-    }
-
-    @Test
-    public void testEndpointNotProvided() throws Exception {
-
-        thrown.expect(MissingOptionException.class);
-        thrown.expectMessage("Missing required option: e");
-
-        Commands.main(new String[]
-                {       "-u", "me",
-                        "-p", "openSesame",
-                        "-t", "ABAP",
-                        "get-transport-owner",
-                        "-tID", "999"});
-    }
-
-    @Test
-    public void testPasswordNotProvided() throws Exception {
-
-        thrown.expect(MissingOptionException.class);
-        thrown.expectMessage("Missing required option: p");
-
-        Commands.main(new String[]
-                {       "-e", "http://example.org:8000/endpoint",
-                        "-u", "me",
-                        "-t", "ABAP",
-                        "get-transport-owner",
-                        "-tID", "999"});
-    }
 
     @Test
     public void testTransportIdNotProvided() throws Exception {
@@ -108,57 +47,6 @@ public class ABAPTransportTest extends ABAPBackendTest {
                         "-p", "openSesame",
                         "-t", "ABAP",
                         "get-transport-owner"});
-    }
-
-    @Test
-    public void testBackendTypeNotProvided() throws Exception {
-
-        thrown.expect(CMCommandLineException.class);
-        thrown.expectMessage("Cannot retrieve backend type.");
-
-        Commands.main(new String[]
-                {       "-e", "http://example.org:8000/endpoint",
-                        "-u", "me",
-                        "-p", "openSesame",
-                        "get-transport-owner",
-                        "-tID", "999"});
-    }
-
-    @Test
-    public void testInvalidBackendTypeProvided() throws Exception {
-
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("No enum constant");
-        thrown.expectMessage("XXX");
-
-        Commands.main(new String[]
-                {       "-e", "http://example.org:8000/endpoint",
-                        "-u", "me",
-                        "-p", "openSesame",
-                        "-t", "XXX",
-                        "-tID", "999"});
-    }
-
-    @Test
-    public void test_InterstingFinding_TransportIDBeforeBackendTypeDoesNotWork() throws Exception {
-
-        //
-        // In this case "-tID" is understaod like "-t ID".
-        //
-        // Since we document that the generic options needs to be provided first - and with that
-        // before the subcommand and the subcommand options this does not hurt.
-        //
-
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("No enum constant");
-        thrown.expectMessage("ID");
-
-        Commands.main(new String[]
-                {       "-e", "http://example.org:8000/endpoint",
-                        "-u", "me",
-                        "-p", "openSesame",
-                        "-tID", "999",
-                        "-t", "XXX"});
     }
 
     @Test
