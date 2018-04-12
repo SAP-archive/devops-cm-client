138884fb38516da9946fdef401a87c3270e7f26c
Marcus Holl <marcus.holl@sap.com>
2018-04-12 09:29:42 +0200

    Transport modifiable test

---
+++ modules/cli/src/test/java/sap/prd/cmintegration/cli/ABAPIsTransportModifiableTest.java
@@ -0,0 +1,56 @@
+package sap.prd.cmintegration.cli;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import com.google.common.collect.Maps;
+import com.sap.cmclient.dto.Transport;
+
+public class ABAPIsTransportModifiableTest extends ABAPTransportTest {
+
+    @Test
+    public void testGetTransportIsModifiablesForTransportInDevelopmentReturnsTrue() throws Exception {
+
+        Map<String, Object> m = Maps.newHashMap();
+        m.put("Id", "999");
+        m.put("Status", "D"); // In development
+        setMock(setupGetTransportMock(new Transport(m)));
+
+        Commands.main(new String[]
+                {       "-e", "http://example.org:8000/endpoint",
+                        "-u", "me",
+                        "-p", "openSesame",
+                        "-t", "ABAP",
+                        "is-transport-modifiable",
+                        "-tID", "999"});
+
+        assertThat(removeCRLF(IOUtils.toString(result.toByteArray(), "UTF-8")), is(equalTo("true")));
+    }
+
+    @Test
+    public void testGetTransportIsModifiableForReleasedTransportReturnsFalse() throws Exception {
+
+        Map<String, Object> m = Maps.newHashMap();
+        m.put("Id", "999");
+        m.put("Status", "R"); // Released
+        setMock(setupGetTransportMock(new Transport(m)));
+
+        Commands.main(new String[]
+                {       "-e", "http://example.org:8000/endpoint",
+                        "-u", "me",
+                        "-p", "openSesame",
+                        "-t", "ABAP",
+                        "is-transport-modifiable",
+                        "-tID", "999"});
+
+        assertThat(removeCRLF(IOUtils.toString(result.toByteArray(), "UTF-8")), is(equalTo("false")));
+    }
+
+}
+
