ca34c567447713784adfa9268f838fcbc818d2da
Marcus Holl <marcus.holl@sap.com>
2018-04-13 14:28:08 +0200

    Switch to invocation via Commands for get-transport-description

--- modules/cli/src/test/java/sap/prd/cmintegration/cli/SolManBackendGetChangeTransportDescriptionTest.java
+++ modules/cli/src/test/java/sap/prd/cmintegration/cli/SolManBackendGetChangeTransportDescriptionTest.java
@@ -13,12 +13,12 @@ public class SolManBackendGetChangeTransportDescriptionTest extends SolManBacken
     public void getChangeTransportDesciptionStraightForward() throws Exception {
 
         setMock(setupMock("L21K900026", "xOwner", "xDescription", false));
-        GetTransportDescriptionSOLMAN.main(new String[] {
+        Commands.main(new String[] {
                 "-u", SERVICE_USER,
                 "-p", SERVICE_PASSWORD,
                 "-e", SERVICE_ENDPOINT,
                 "-t", "SOLMAN",
-                "dummy-cmd",
+                "get-transport-description",
                 "-cID", "8000038673", "-tID", "L21K900026"});
 
         assertThat(removeCRLF(IOUtils.toString(result.toByteArray(), "UTF-8")),
