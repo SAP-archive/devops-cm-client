33993d1b0d721ef85dba76f2dad5900e57a51889
Marcus Holl <marcus.holl@sap.com>
2018-04-13 14:30:36 +0200

    Switch to invocation via Commands for get-transport-owner

--- modules/cli/src/test/java/sap/prd/cmintegration/cli/SolManBackendGetChangeTransportOwnerTest.java
+++ modules/cli/src/test/java/sap/prd/cmintegration/cli/SolManBackendGetChangeTransportOwnerTest.java
@@ -13,12 +13,12 @@ public class SolManBackendGetChangeTransportOwnerTest extends SolManBackendCMTra
     public void getChangeTransportOwnerStraightForward() throws Exception {
 
         setMock(setupMock("L21K900026", "xOwner", "xDesc", false));
-        GetTransportOwnerSOLMAN.main(new String[] {
+        Commands.main(new String[] {
                 "-u", SERVICE_USER,
                 "-p", SERVICE_PASSWORD,
                 "-e", SERVICE_ENDPOINT,
                 "-t", "SOLMAN",
-                "dummy-cmd",
+                "get-transport-owner",
                 "-cID" ,"8000038673", "-tID", "L21K900026"});
 
         assertThat(removeCRLF(IOUtils.toString(result.toByteArray(), "UTF-8")),
